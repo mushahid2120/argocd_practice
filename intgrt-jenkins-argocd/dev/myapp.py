47bf3567a6e1c0aae047d34cd50507522d1a1773
root <root@ip-172-31-29-56.eu-north-1.compute.internal>
2025-02-14 07:10:36 +0000

    python changed

--- intgrt-jenkins-argocd/dev/myapp.py
+++ intgrt-jenkins-argocd/dev/myapp.py
@@ -2,7 +2,7 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/")
 def home():
-    return "This is Jenkins<--------------------->ArgoCd"
+    return "This is Jenkins<--------------------->ArgoCd Working Awesome"
 
 app.run(port=80,host='0.0.0.0')
 
